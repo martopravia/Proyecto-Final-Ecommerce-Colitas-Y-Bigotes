c2f50466e0440a420906b9982ce4e75022f3dd67
Martín Pravia <marto.pravia@gmail.com>
2024-12-16 18:06:13 +0000

    cargo labels para nombre, apellido, celular y domicilio

--- src/front/js/pages/Profile.jsx
+++ src/front/js/pages/Profile.jsx
@@ -8,13 +8,59 @@ const Profile = () => {
           <div className="col">Profile</div>
         </div>
 
-        <div className="row justify-content-center mt-3">
-          <div className="col-3">
+        <div className="row mt-3">
+          <div className="col-4">
             <img
-              src="https://picsum.photos/id/40/50/50"
+              src="https://picsum.photos/id/40/200/200"
               className="img-thumbnail"
               alt="..."
             />
+          </div>
+          <div className="col-4">
+          <div className="mb-3">
+  <label htmlFor="exampleFormControlInput1" className="form-label">
+    Nombre
+  </label>
+  <input
+    type="text"
+    className="form-control"
+    id="exampleFormControlInput1"
+    placeholder="Ingrese su nombre..."
+  />
+  <label htmlFor="exampleFormControlInput1" className="form-label mt-5">
+    Apellido
+  </label>
+  <input
+    type="text"
+    className="form-control"
+    id="exampleFormControlInput1"
+    placeholder="Ingrese su apellido..."
+  />
+</div>
+
+          </div>
+          <div className="col-4">
+          <div className="mb-3">
+  <label htmlFor="exampleFormControlInput1" className="form-label">
+    Celular
+  </label>
+  <input
+    type="tel"
+    className="form-control"
+    id="exampleFormControlInput1"
+    placeholder="Ingrese su celular..."
+  />
+  <label htmlFor="exampleFormControlInput1" className="form-label mt-5">
+    Domicilio completo
+  </label>
+  <input
+    type="text"
+    className="form-control"
+    id="exampleFormControlInput1"
+    placeholder="Ingrese su domicilio completo..."
+  />
+</div>
+
           </div>
         </div>
       </div>
