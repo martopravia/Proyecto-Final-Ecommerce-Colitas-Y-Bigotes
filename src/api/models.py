dcbc6fb09da3e3f09176cfebe72d8d64fe3c67d0
Martín Pravia <marto.pravia@gmail.com>
2024-12-19 15:24:02 +0000

    perfilado db

--- src/api/models.py
+++ src/api/models.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, Boolean, Text, Float, DateTime, ForeignKey, Table
+from sqlalchemy import relationship
+
 
 db = SQLAlchemy()
 
@@ -12,7 +14,8 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     # is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
+    order = db.relationship("Order", backref="user")
+    
     def _repr_(self):
         return f'<User {self.email}>'
 
@@ -32,8 +35,9 @@ class Order(db.Model):
      deliver_address = db.Column(db.String(180), nullable=False)
      status = db.Column(db.String(180), nullable=False)
      user_id = db.Column(db.Integer, ForeignKey("users.id"))
+     order_detail = db.relationship("OrderDetail", backref = "order")
      
-class Order_Detail(db.Model):
+class OrderDetail(db.Model):
     __tablename__ = "order_detail"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False) 
     order_id = db.Column(db.Integer, ForeignKey("orders.id"), primary_key=True, nullable=False)
@@ -50,18 +54,21 @@ class Product(db.Model):
     quantity = db.Column(db.Integer, nullable=False)
     category_id = db.Column(db.Integer, ForeignKey("categories.id"), nullable=False)
     subcategory_id = db.Column(db.Integer, ForeignKey("subcategories.id"), nullable=False)
+    order_detail = db.relationship("OrderDetail", backref="product")
 
 class Category(db.Model):
     __tablename__ = "categories"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     name = db.Column(db.String(120), nullable=False)
+    product = db.relationship("Product", backref="category")
+    subcategory = db.relationship("Subcategory", backref="category")
     
 class Subcategory(db.Model):
     __tablename__ = "subcategories"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     name = db.Column(db.String(120), nullable=False)
     category_id = db.Column(db.Integer, ForeignKey("categories.id"), nullable=False)
-    
+    product =db.relationship("Product", backref="subcategory")
 
      
      
