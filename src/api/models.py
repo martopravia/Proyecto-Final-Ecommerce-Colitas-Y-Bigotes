e537449d965e30aef30678dd65bbe25b8b7573ca
Martín Pravia <marto.pravia@gmail.com>
2024-12-19 17:55:38 +0000

    base de datos

--- src/api/models.py
+++ src/api/models.py
@@ -1,20 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, Boolean, Text, Float, DateTime, ForeignKey, Table
-from sqlalchemy import relationship
-
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    _tablename_ = "users"
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(120), nullable=False)
     lastname = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     # is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-    order = db.relationship("Order", backref="user")
+    orders = db.relationship("Order", backref="user")
     
     def _repr_(self):
         return f'<User {self.email}>'
@@ -27,21 +24,21 @@ class User(db.Model):
         }
         
 class Order(db.Model):
-     _tablename_ = "orders"
+     __tablename__ = "orders"
      id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
      date = db.Column(db.DateTime, default=datetime.now, nullable=False)
      amount = db.Column(db.Float, nullable=False)
      address = db.Column(db.String(180), nullable=False)
      deliver_address = db.Column(db.String(180), nullable=False)
      status = db.Column(db.String(180), nullable=False)
-     user_id = db.Column(db.Integer, ForeignKey("users.id"))
-     order_detail = db.relationship("OrderDetail", backref = "order")
+     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+     order_details = db.relationship("OrderDetail", backref = "order")
      
 class OrderDetail(db.Model):
     __tablename__ = "order_detail"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False) 
-    order_id = db.Column(db.Integer, ForeignKey("orders.id"), primary_key=True, nullable=False)
-    product_id = db.Column(db.Integer, ForeignKey("products.id"), nullable=False)
+    order_id = db.Column(db.Integer, db.ForeignKey("orders.id"), primary_key=True, nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey("products.id"), nullable=False)
     quantity = db.Column(db.Integer, nullable=False) 
     amount = db.Column(db.Float, nullable=False)
 
@@ -51,8 +48,8 @@ class Product(db.Model):
     name = db.Column(db.String(120), nullable=False)
     photo = db.Column(db.String(200), nullable=False)
     amount = db.Column(db.Float, nullable=False)
-    category_id = db.Column(db.Integer, ForeignKey("categories.id"), nullable=False)
-    subcategory_id = db.Column(db.Integer, ForeignKey("subcategories.id"), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"), nullable=False)
+    subcategory_id = db.Column(db.Integer, db.ForeignKey("subcategories.id"), nullable=False)
     order_detail = db.relationship("OrderDetail", backref="product")
     stock = db.relationship("Stock", backref="product")
     
@@ -68,13 +65,20 @@ class Subcategory(db.Model):
     __tablename__ = "subcategories"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     name = db.Column(db.String(120), nullable=False)
-    category_id = db.Column(db.Integer, ForeignKey("categories.id"), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"), nullable=False)
     product =db.relationship("Product", backref="subcategory")
 
 class Stock(db.Model):
     __tablename__ = "stock"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-    products_id = db.Column(db.Integer, ForeignKey("products.id"), nullable=False)
+    products_id = db.Column(db.Integer, db.ForeignKey("products.id"), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
     date_in = db.Column(db.DateTime, default=datetime.now, nullable=False)
      
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+        
