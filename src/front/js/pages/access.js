60f007dafc78f58ecd36433e19d948ee51d84999
Martín Pravia <marto.pravia@gmail.com>
2024-12-30 18:12:42 +0000

    se realiza acceso a la app, una vez logeado va a home

--- src/front/js/pages/access.js
+++ src/front/js/pages/access.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
+import { Navigate } from "react-router-dom";
 
 export const Access = () => {
+    const { store, actions } = useContext(Context)
+
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loggedIn, setLoggedIn] = useState("");
+
+    if (store.isLogged) return <Navigate to="/" replace />
+
+
+
+
     return (
         <>
 
-            <form className="register-form d-flex flex-column bg-light aling-items-center text-center mx-auto my-5" action="#" method="GET">
+            <form className="register-form d-flex flex-column bg-light aling-items-center text-center mx-auto my-5" action="#" onSubmit={actions.handleSubmit} method="GET">
                 <h1 className="h1"> ACCEDER </h1>
-                <label className="register-label" for="user_name"> Email<span className="asq"> * </span></label>
-                <input type="text" id="user_name" className="bg-light register-input" placeholder="Ingrese su email..."/>
+                <label className="reg0ister-label" for="user_name"> Email<span className="asq"> * </span></label>
+                <input type="text" id="email" className="bg-light register-input" placeholder="Ingrese su email..." />
                 <label className="register-label" for="password">Contraseña <span className="asq"> * </span></label>
-                <input type="password" id="password" className="bg-light register-input" placeholder="Ingrese su contraseña..."/>
+                <input type="password" id="password" className="bg-light register-input" placeholder="Ingrese su contraseña..." />
                 <button className="bg-dark w-25 rounded-4 p-1 mx-auto text-light fs-5 mt-3" type="submit"> Ingresar </button>
                 <div className="d-flex justify-content-between w-100 mt-2 px-3 ">
                     <div>
@@ -19,8 +33,11 @@ export const Access = () => {
                     <a href="#" className="text-decoration-none fs-5">¿Olvidaste tu contraseña?</a>
                 </div>
             </form>
+
+
         </>
     )
 }
+
 
 export default Access
